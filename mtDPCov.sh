53bf651b48eb3019be5320fd35308c6255716a4e
Mahautgr <99719354+Gaia32@users.noreply.github.com>
2023-07-19 08:55:08 +0100

    Create extractMtDPCov.sh

---
+++ mtDPCov.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=RprtDPCov
+#SBATCH --output=BWA6DepthCoverageMT.out
+
+DirRaw=(path/to/your/bam/files)
+
+################################
+### SAMTOOLS
+### this script allows to extract reads mapping over the mitochondiral chromosome
+### it also allows to extract the average coverage and depth of each sample in the output file BWA6DepthCoverageMT.out
+source /local/env/envsamtools-1.15.sh #<-- /!\ change here the path to the samtool software / environment
+################################
+
+for file in ${DirRaw}*.bam; do
+	name=${file##*/}
+	id=${name%_{you can put here everything you dont want in the name}.bam} # <-- /!\
+
+    samtools view $DirRaw${id}*.bam MT -o ${id}_MT.bam
+    samtools coverage ${id}_MT.bam > ${id}_samcoverage.txt
+
+	  echo "Sample_Id,"$id
+	  echo -n "Coverage," ; awk 'FNR==2 {print $6}' ${id}_samcoverage.txt
+	  echo -n "Depth," ; awk 'FNR==2 {print $7}' ${id}_samcoverage.txt
+
+done
+
